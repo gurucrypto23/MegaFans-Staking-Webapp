c877c681cecf9098008082480fde75c2a60a98fb
gurucrypto23 <justinliu0623@gmail.com>
2022-06-09 02:20:20 -0700

    Add func

--- src/components/NFTBalance.jsx
+++ src/components/NFTBalance.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { } from "react";
 import { useMoralis, useNFTBalances } from "react-moralis";
-import { Card, Image, Tooltip, Modal, Input, Skeleton } from "antd";
+import { Card, Image, Tooltip, Skeleton } from "antd";
 import {
   SendOutlined,
 } from "@ant-design/icons";
-// import { getExplorer } from "helpers/networks";
-import AddressInput from "./AddressInput";
+import { abi as megaFansAbi } from "../contracts/MegaFansNFT.json";
 import address from "../addresses/address";
 
 const { Meta } = Card;
@@ -26,44 +25,23 @@ const styles = {
 function NFTBalance() {
   const { data: NFTBalances } = useNFTBalances();
   const { Moralis } = useMoralis();
-  const [visible, setVisibility] = useState(false);
-  const [receiverToSend, setReceiver] = useState(null);
-  const [amountToSend, setAmount] = useState(null);
-  const [nftToSend, setNftToSend] = useState(null);
-  const [isPending, setIsPending] = useState(false);
 
-  async function transfer(nft, amount, receiver) {
-    const options = {
-      type: nft.contract_type,
-      tokenId: nft.token_id,
-      receiver: receiver,
-      contractAddress: nft.token_address,
-    };
-
-    if (options.type === "erc1155") {
-      options.amount = amount;
-    }
-
-    setIsPending(true);
-    await Moralis.transfer(options)
-      .then((tx) => {
-        console.log(tx);
-        setIsPending(false);
-      })
-      .catch((e) => {
-        alert(e.message);
-        setIsPending(false);
-      });
+  async function getNFT(token_id) {
+    return await Moralis.executeFunction({
+      functionName: "tokenURI",
+      megaFansAbi,
+      contractAddress: address.mumbai.MEGAFANSNFT_ADDRESS,
+      params: {
+        tokenId: token_id
+      }
+    });
   }
 
   const nftStaking = (nft) => {
-    setNftToSend(nft);
-    setVisibility(true);
+    // setNftToSend(nft);
+    // setVisibility(true);
   };
 
-  const handleChange = (e) => {
-    setAmount(e.target.value);
-  };
 
   return (
     <div style={{ padding: "15px", maxWidth: "1030px", width: "100%" }}>
@@ -72,9 +50,9 @@ function NFTBalance() {
         <Skeleton loading={!NFTBalances?.result}>
           {NFTBalances?.result &&
             NFTBalances.result.map((nft, index) => {
-              console.log(address.mumbai.MEGAFANSNFT_ADDRESS, nft.token_address.toLowerCase());
               if (nft.token_address.toLowerCase() === address.mumbai.MEGAFANSNFT_ADDRESS.toLowerCase()) {
                 console.log(nft);
+                console.log(getNFT(1));
                 return (
                   <Card
                     hoverable
@@ -116,22 +94,6 @@ function NFTBalance() {
             })}
         </Skeleton>
       </div>
-      <Modal
-        title={`Transfer ${nftToSend?.name || "NFT"}`}
-        visible={visible}
-        onCancel={() => setVisibility(false)}
-        onOk={() => transfer(nftToSend, amountToSend, receiverToSend)}
-        confirmLoading={isPending}
-        okText="Send"
-      >
-        <AddressInput autoFocus placeholder="Receiver" onChange={setReceiver} />
-        {nftToSend && nftToSend.contract_type === "erc1155" && (
-          <Input
-            placeholder="amount to send"
-            onChange={(e) => handleChange(e)}
-          />
-        )}
-      </Modal>
     </div>
   );
 }

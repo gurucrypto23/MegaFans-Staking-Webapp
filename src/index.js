179939468c0d078eef4806966803fa5894d6de3b
gurucrypto23 <justinliu0623@gmail.com>
2022-06-09 13:20:09 -0700

    update the index'

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import "./index.css";
 import QuickStart from "components/Home";
 
 /** Get your free Moralis Account https://moralis.io/ */
-
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
 // const APP_ID="imAomu1AxniYv9w4fYeC11nOkoHwZuAGOmbxCImB";

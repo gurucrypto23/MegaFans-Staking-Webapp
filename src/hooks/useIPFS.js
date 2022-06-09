a2ea5a55594e2778913ecc92adc91e2397395332
gurucrypto23 <justinliu0623@gmail.com>
2022-06-08 23:26:02 -0700

    IPFS file update

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -33,7 +33,7 @@ export const useIPFS = () => {
     // const file = await saveFile("file.json", {saveIPFS: true, base64 : Buffer.from(JSON.stringify(metadata), 'base64')});
     // const buff = Buffer.from(JSON.stringify(metadata), 'base64')
     const buff = btoa(JSON.stringify(metadata))
-    const file = await saveFile("file.json", {base64: buff}, {saveIPFS: true});
+    const file = await saveFile("0x00.json", {base64: buff}, {saveIPFS: true});
     return file.hash();
   };
 
